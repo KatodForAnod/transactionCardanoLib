581545d333b158d7a1bcc79aaea489877834bc13
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-01-03 13:58:35 +0300

    added transaction_op.go

---
+++ policy/transaction_op.go
@@ -0,0 +1,49 @@
+package policy
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"transactionCardanoLib/dto"
+)
+
+const transactionSignTmpl = "cardano-cli transaction sign " +
+	"--signing-key-file payment.skey  " +
+	"--signing-key-file %s " +
+	"--testnet-magic %s " +
+	"--tx-body-file matx.raw  " +
+	"--out-file matx.signed"
+
+func TransactionSign(id string, tokenStruct dto.TokenStruct) error {
+	comm := fmt.Sprintf(transactionSignTmpl, tokenStruct.PolicySigningFilePath, id)
+
+	err := exec.Command(comm).Run()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+const transactionBuildTmpl = "cardano-cli transaction build-raw " +
+	"--fee %s " +
+	"--tx-in $%s#$%s " +
+	"--tx-out $%s+$%s+\"$%s $%s + $%s $%s\" " +
+	"--mint=\"$%s $%s + $%s $%s\" " +
+	"--minting-script-file %s " +
+	"--out-file matx.raw"
+
+func TransactionBuild(fee, txHash, txIx, address, output, tokenAmount,
+	tokenName1, tokenName2, policyScriptFilePath string) error {
+	comm := fmt.Sprintf(transactionBuildTmpl, fee, txHash, txIx, address, output, tokenAmount, tokenName1,
+		tokenAmount, tokenName2, tokenAmount, tokenName1, tokenAmount, tokenName2, policyScriptFilePath)
+
+	err := exec.Command(comm).Run()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}

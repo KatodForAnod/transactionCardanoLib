a164cc655ab65d4a4cd5d616eed794e34f54b477
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-01-03 13:32:15 +0300

    change dto struct

--- dto/token_struct.go
+++ dto/token_struct.go
@@ -1,13 +1,11 @@
 package dto
 
-import "os"
-
 type TokenStruct struct {
-	TokenName              string   `json:"token_name"`
-	TokenAmount            int64    `json:"token_amount"`
-	PaymentAddress         string   `json:"payment_address"`
-	UsingExistingPolicy    bool     `json:"using_existing_policy"`
-	PolicyScriptFile       *os.File `json:"policy_script_file"`
-	PolicySigningFile      *os.File `json:"policy_signing_file"`
-	PolicyVerificationFile *os.File `json:"policy_verification_file"`
+	TokenName                  string `json:"token_name"`
+	TokenAmount                int64  `json:"token_amount"`
+	PaymentAddress             string `json:"payment_address"`
+	UsingExistingPolicy        bool   `json:"using_existing_policy"`
+	PolicyScriptFilePath       string `json:"policy_script_file_path"`
+	PolicySigningFilePath      string `json:"policy_signing_file_path"`
+	PolicyVerificationFilePath string `json:"policy_verification_file_path"`
 }

df7214a8c1139c8ea5856d7abe08582a98fa479f
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-04-17 15:10:28 +0300

    new config struct

--- config/config.go
+++ config/config.go
@@ -5,21 +5,20 @@ import (
 	"log"
 )
 
-// Config - structure of config file
 type Config struct {
-	Token TokenStruct `json:"token"`
-}
-
-type TokenStruct struct {
 	ID                         string `json:"id"`
-	TokenName                  string `json:"token_name"`
-	TokenAmount                int64  `json:"token_amount"`
+	Token                      Token  `json:"token"`
 	PaymentAddress             string `json:"payment_address"`
 	UsingExistingPolicy        bool   `json:"using_existing_policy"`
 	PolicyID                   string `json:"policy_id"`
 	PolicyScriptFilePath       string `json:"policy_script_file_path"`
 	PolicySigningFilePath      string `json:"policy_signing_file_path"`
 	PolicyVerificationFilePath string `json:"policy_verification_file_path"`
+}
+
+type Token struct {
+	TokenName   string `json:"token_name"`
+	TokenAmount int64  `json:"token_amount"`
 }
 
 const cConfigPath = "conf.config"

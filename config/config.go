6bfff749066722c2480f528af8c143aed75b8515
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-04-17 18:12:58 +0300

    change config struct

--- config/config.go
+++ config/config.go
@@ -6,14 +6,14 @@ import (
 )
 
 type Config struct {
-	ID                         string `json:"id"`
-	Token                      Token  `json:"token"`
-	PaymentAddress             string `json:"payment_address"`
-	UsingExistingPolicy        bool   `json:"using_existing_policy"`
-	PolicyID                   string `json:"policy_id"`
-	PolicyScriptFilePath       string `json:"policy_script_file_path"`
-	PolicySigningFilePath      string `json:"policy_signing_file_path"`
-	PolicyVerificationFilePath string `json:"policy_verification_file_path"`
+	ID                         string  `json:"id"`
+	Token                      []Token `json:"token"`
+	PaymentAddress             string  `json:"payment_address"`
+	UsingExistingPolicy        bool    `json:"using_existing_policy"`
+	PolicyID                   string  `json:"policy_id"`
+	PolicyScriptFilePath       string  `json:"policy_script_file_path"`
+	PolicySigningFilePath      string  `json:"policy_signing_file_path"`
+	PolicyVerificationFilePath string  `json:"policy_verification_file_path"`
 }
 
 type Token struct {

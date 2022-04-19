1d78ead64e327d69e3ee15efe06353a482459fd3
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-04-19 11:53:16 +0300

    2 new field paymnetSkey/vkey

--- config/config.go
+++ config/config.go
@@ -6,14 +6,17 @@ import (
 )
 
 type Config struct {
-	ID                         string  `json:"id"`
-	Token                      []Token `json:"token"`
-	PaymentAddress             string  `json:"payment_address"`
-	UsingExistingPolicy        bool    `json:"using_existing_policy"`
-	PolicyID                   string  `json:"policy_id"`
-	PolicyScriptFilePath       string  `json:"policy_script_file_path"`
-	PolicySigningFilePath      string  `json:"policy_signing_file_path"`
-	PolicyVerificationFilePath string  `json:"policy_verification_file_path"`
+	ID                  string  `json:"id"` //1
+	Token               []Token `json:"token"`
+	PaymentAddress      string  `json:"payment_address"`         //2
+	PaymentSKeyFilePath string  `json:"payment_s_key_file_path"` //3
+	PaymentVKeyFilePath string  `json:"payment_v_key_file_path"` //4
+
+	UsingExistingPolicy        bool   `json:"using_existing_policy"`
+	PolicyID                   string `json:"policy_id"`
+	PolicyScriptFilePath       string `json:"policy_script_file_path"`
+	PolicySigningFilePath      string `json:"policy_signing_file_path"`
+	PolicyVerificationFilePath string `json:"policy_verification_file_path"`
 }
 
 type Token struct {

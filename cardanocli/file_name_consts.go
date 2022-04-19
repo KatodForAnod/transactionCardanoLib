ec109ccdcc37d2dd3b6cb5bd48a80923aea417f0
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-04-19 15:51:54 +0300

    new file for build transaction send token

--- cardanocli/file_name_consts.go
+++ cardanocli/file_name_consts.go
@@ -1,11 +1,13 @@
 package cardanocli
 
 var (
-	PolicyDirName             = "policy"
-	RawTransactionFile        = "matx.raw"                       // Raw transaction to mint token
-	SignedTransactionFile     = "matx.signed"                    // Signed transaction to mint token
-	MetadataAttrFile          = "metadata.json"                  // Metadata to specify NFT attributes
-	PaymentAddrFile           = "payment.addr"                   // Address to send/receive
+	PolicyDirName         = "policy"
+	RawTransactionFile    = "matx.raw"    // Raw transaction to mint token
+	SignedTransactionFile = "matx.signed" // Signed transaction to mint token
+
+	MetadataAttrFile = "metadata.json" // Metadata to specify NFT attributes
+	PaymentAddrFile  = "payment.addr"  // Address to send/receive
+
 	PaymentSignKeyFile        = "payment.skey"                   // Payment signing key
 	PaymentVerifyKeyFile      = "payment.vkey"                   // Payment verification key
 	PolicyScriptFile          = PolicyDirName + "/policy.script" // Script to generate the policyID
@@ -13,4 +15,6 @@ var (
 	PolicyVerificationkeyFile = PolicyDirName + "/policy.vkey"   // Policy verification key
 	PolicyIDFile              = PolicyDirName + "/policyID"      // File which holds the policy ID
 	ProtocolParametersFile    = "protocol.json"                  // Protocol parameters
+
+	RawTransactionSendTokenFile = "rec_matx.raw"
 )

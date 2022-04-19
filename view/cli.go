f550f7ab16b1fcc7d462c96f8fb393c6d301a2bc
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-04-19 13:16:23 +0300

    change cli - added new chose send token

--- view/cli.go
+++ view/cli.go
@@ -19,10 +19,16 @@ const (
 	showCardanoUtxo     = 3
 	submitTransaction   = 4
 	generatePolicyFiles = 5
+	createTokens        = 6
+	sendTokens          = 7
 )
 
 var (
-	startMsg = fmt.Sprintf(
+	startMsg = fmt.Sprintf("%d. Create tokens\n"+
+		"%d. Send tokens\n"+
+		"%d. Exit\n",
+		createTokens, sendTokens, exitCommand)
+	transactionOpMsg = fmt.Sprintf(
 		"%d. Build transaction\n"+
 			"%d. Sign transaction\n"+
 			"%d. Show cardano utxo\n"+
@@ -51,9 +57,8 @@ func (f *Frontend) SetConfAndCardanoLib(conf config.Config,
 }
 
 func (f *Frontend) Start() error {
-	fmt.Print(startMsg)
-
 	for {
+		fmt.Print(startMsg)
 		var choiceCommand int
 		if _, err := fmt.Scan(&choiceCommand); err != nil {
 			log.Println(err)
@@ -64,14 +69,28 @@ func (f *Frontend) Start() error {
 			return nil
 		}
 
-		if err := f.switcher(choiceCommand); err != nil {
-			log.Println(err)
-			return err
+		for {
+			fmt.Print(transactionOpMsg)
+			if _, err := fmt.Scan(&choiceCommand); err != nil {
+				log.Println(err)
+				return err
+			}
+			if choiceCommand == exitCommand {
+				break
+			}
+			switch choiceCommand {
+			case createTokens:
+				if err := f.switcherCreateTokens(choiceCommand); err != nil {
+					log.Println(err)
+					return err
+				}
+			case sendTokens:
+			}
 		}
 	}
 }
 
-func (f *Frontend) switcher(command int) error {
+func (f *Frontend) switcherCreateTokens(command int) error {
 	switch command {
 	case buildTransaction:
 		cliOut, errOutput, err := f.cardanoLib.CardanoQueryUtxo()
@@ -163,3 +182,7 @@ func (f *Frontend) switcher(command int) error {
 
 	return nil
 }
+
+/*func (f *Frontend) switcherSendTokens(command int) error {
+
+}*/
